62a62240ea4b786a5179f5df438d4ff9e04ba2ce
Anissa Billah <anissabillah0805@gmail.com>
2019-11-16 19:10:59 +0700

    add: model card

---
+++ app/src/main/java/com/example/cula_mobile/model/Card.java
@@ -0,0 +1,56 @@
+package com.example.cula_mobile.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Card {
+    @SerializedName("id")
+    private int idCard;
+    @SerializedName("id_project")
+    private int idProject;
+    @SerializedName("name")
+    private String cardName;
+    @SerializedName("created_at")
+    private String createdDate;
+    @SerializedName("updated_at")
+    private String updatedDate;
+
+    public int getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(int idCard) {
+        this.idCard = idCard;
+    }
+
+    public int getIdProject() {
+        return idProject;
+    }
+
+    public void setIdProject(int idProject) {
+        this.idProject = idProject;
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(String updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+}
