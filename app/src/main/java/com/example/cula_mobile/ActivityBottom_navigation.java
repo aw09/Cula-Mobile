c03cdc9644b7c6738c2c8d571636cbe178e49d1a
Anissa Billah <anissabillah0805@gmail.com>
2019-11-25 23:30:48 +0700

    update: bottom navigation

--- app/src/main/java/com/example/cula_mobile/ActivityBottom_navigation.java
+++ app/src/main/java/com/example/cula_mobile/ActivityBottom_navigation.java
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.example.cula_mobile.module.mytask.MyCardFragment;
+import com.example.cula_mobile.module.project.ProjectsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ActivityBottom_navigation extends AppCompatActivity {
@@ -24,16 +25,12 @@ public class ActivityBottom_navigation extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_myprojects:
-                    actionBar.setTitle("Project");
                     return fragmentTransaction(new ProjectsFragment());
                 case R.id.navigation_mytask:
-                    actionBar.setTitle("My Task");
                     return fragmentTransaction(new MyCardFragment());
                 case R.id.navigation_notifications:
-                    actionBar.setTitle("Notification");
                     return fragmentTransaction(new NotificationsFragment());
                 case R.id.navigation_account:
-                    actionBar.setTitle("Account");
                     return fragmentTransaction(new AccountFragment());
             }
             return false;
@@ -41,13 +38,19 @@ public class ActivityBottom_navigation extends AppCompatActivity {
     };
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        fragmentTransaction(new ProjectsFragment());
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
-        fragmentTransaction(new MyCardFragment());
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        mTextMessage = findViewById(R.id.message);
+
 
     }
 
@@ -57,7 +60,6 @@ public class ActivityBottom_navigation extends AppCompatActivity {
         fragmentManager
                 .beginTransaction()
                 .replace(R.id.frame_layout_container, fragment, "")
-                .addToBackStack(backStackStateName)
                 .commit();
         return true;
     }
