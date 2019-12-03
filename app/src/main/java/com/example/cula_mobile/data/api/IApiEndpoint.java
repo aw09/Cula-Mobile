38bb5e82eb58ca0b2d70e0dfc74f291f1dd0466f
Anissa Billah <anissabillah0805@gmail.com>
2019-12-03 08:29:59 +0700

    update: api endpoint

--- app/src/main/java/com/example/cula_mobile/data/api/IApiEndpoint.java
+++ app/src/main/java/com/example/cula_mobile/data/api/IApiEndpoint.java
@@ -5,8 +5,10 @@ import com.example.cula_mobile.model.Card;
 import com.example.cula_mobile.model.Project;
 import com.example.cula_mobile.model.Task;
 import com.example.cula_mobile.model.User;
+import com.example.cula_mobile.model.response.ResponseBoard;
 import com.example.cula_mobile.model.response.ResponseCreateTask;
 import com.example.cula_mobile.model.response.ResponseLogin;
+import com.example.cula_mobile.model.response.ResponseMyTask;
 
 import java.util.ArrayList;
 
@@ -27,7 +29,7 @@ public interface IApiEndpoint {
     );
 
     @GET("myTask")
-    Call<ArrayList<Task>> myTask(
+    Call<ArrayList<ResponseMyTask>> myTask(
             @Header("Authorization") String header
     );
 
@@ -36,14 +38,14 @@ public interface IApiEndpoint {
             @Header("Authorization") String header
     );
 
-    @GET("show-project/{id}")
+    @GET("project/{id}")
     Call<ArrayList<Board>> showProject(
             @Header("Authorization") String header,
             @Path("id") int id
     );
 
     @GET("board/{id}")
-    Call<ArrayList<Card>> showBoard(
+    Call<ResponseBoard> showBoard(
             @Header("Authorization") String header,
             @Path("id") int id
     );
@@ -61,5 +63,11 @@ public interface IApiEndpoint {
             @Field("task") String taskName,
             @Field("description_of_task") String description,
             @Field("due_date") String dueDate
+    );
+
+    @GET("show-task/{id}")
+    Call<Task> detailTask(
+            @Header("Authorization") String header,
+            @Path("id") int id
     );
 }
