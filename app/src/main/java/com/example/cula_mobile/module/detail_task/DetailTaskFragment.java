3067afcc198231297df0a441c97661dfe48dcc85
Anissa Billah <anissabillah0805@gmail.com>
2019-12-26 17:37:26 +0700

    update: detailTask

--- app/src/main/java/com/example/cula_mobile/module/detail_task/DetailTaskFragment.java
+++ app/src/main/java/com/example/cula_mobile/module/detail_task/DetailTaskFragment.java
@@ -1,11 +1,11 @@
 package com.example.cula_mobile.module.detail_task;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -14,12 +14,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.cula_mobile.R;
 import com.example.cula_mobile.model.Comment;
+import com.example.cula_mobile.model.Label;
 import com.example.cula_mobile.model.Subtask;
 import com.example.cula_mobile.model.response.ResponseDetailTask;
 import com.example.cula_mobile.module.sub_task.CreateSubtaskFragment;
@@ -35,6 +39,9 @@ public class DetailTaskFragment extends Fragment implements IDetailTaskView{
     private DetailTaskPresenter detailTaskPresenter;
     private TextView txtDescription, txtDueDate, txtInfo;
     private ImageButton btnAddSubtask;
+    private Button addComment;
+    private Fragment fragment;
+    private LinearLayout layout_label;
 
     public DetailTaskFragment(int idTask) {
         // Required empty public constructor
@@ -48,16 +55,25 @@ public class DetailTaskFragment extends Fragment implements IDetailTaskView{
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.activity_detail_task_2, container, false);
 
-        detailTaskPresenter = new DetailTaskPresenter(this);
-        detailTaskPresenter.getDetailTask(idTask);
+        detailTaskPresenter = new DetailTaskPresenter(this, idTask);
+        detailTaskPresenter.getDetailTask();
 
         setStatus();
+        layout_label = (LinearLayout) view.findViewById(R.id.cardLabel);
 
         btnAddSubtask = (ImageButton) view.findViewById(R.id.btnAddSubtask);
         btnAddSubtask.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 moveToCreateSubtask();
+            }
+        });
+
+        addComment = (Button) view.findViewById(R.id.btnAddComment);
+        addComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createComment();
             }
         });
 
@@ -100,6 +116,25 @@ public class DetailTaskFragment extends Fragment implements IDetailTaskView{
         actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    public void showLabel(Label label) {
+        TextView labelName = (TextView) view.findViewById(R.id.labelName);
+        labelName.setText(label.getLabelName());
+
+        if (label.getColorLabel().toLowerCase().equals("red")) {
+            layout_label.setBackgroundColor(Color.parseColor("#FF748F"));
+            labelName.setTextColor(Color.WHITE);
+        } else if (label.getColorLabel().toLowerCase().equals("blue")) {
+            layout_label.setBackgroundColor(Color.parseColor("#0F9CDB"));
+            labelName.setTextColor(Color.WHITE);
+        }
+    }
+
+    @Override
+    public void reload() {
+
+    }
+
     private void setStatus() {
         String[] status = {"To Do", "On progress", "Done"};
         Spinner spinner = (Spinner) view.findViewById(R.id.spinnerStatus);
@@ -107,11 +142,17 @@ public class DetailTaskFragment extends Fragment implements IDetailTaskView{
     }
 
     private void moveToCreateSubtask() {
-        Fragment fragment = new CreateSubtaskFragment(idTask);
+        fragment = new CreateSubtaskFragment(idTask);
         FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
         fragmentManager.beginTransaction()
                 .replace(R.id.frame_layout_container, fragment)
                 .addToBackStack(null)
                 .commit();
+    }
+
+    private void createComment() {
+        EditText content = view.findViewById(R.id.addComment);
+        detailTaskPresenter.addComment(content.getText().toString());
+
     }
 }
--- app/src/main/java/com/example/cula_mobile/module/detail_task/IDetailTaskView.java
+++ app/src/main/java/com/example/cula_mobile/module/detail_task/IDetailTaskView.java
@@ -1,6 +1,7 @@
 package com.example.cula_mobile.module.detail_task;
 
 import com.example.cula_mobile.model.Comment;
+import com.example.cula_mobile.model.Label;
 import com.example.cula_mobile.model.Subtask;
 import com.example.cula_mobile.model.Task;
 import com.example.cula_mobile.model.response.ResponseDetailTask;
@@ -12,4 +13,6 @@ public interface IDetailTaskView {
     void showSubtask(ArrayList<Subtask> subtasks);
     void showComment(ArrayList<Comment> comments);
     void showTaskTitle(String taskTitle);
+    void showLabel(Label labels);
+    void reload();
 }
