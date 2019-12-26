9ebb5376bb4ee7b179c4a65744d7e54ebff9d13b
Anissa Billah <anissabillah0805@gmail.com>
2019-12-26 17:28:06 +0700

    update: layout

--- app/src/main/java/com/example/cula_mobile/module/detail_task/DetailTaskPresenter.java
+++ app/src/main/java/com/example/cula_mobile/module/detail_task/DetailTaskPresenter.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import com.example.cula_mobile.data.api.ApiRetrofit;
 import com.example.cula_mobile.data.api.IApiEndpoint;
+import com.example.cula_mobile.model.Comment;
 import com.example.cula_mobile.model.response.ResponseDetailTask;
 import com.example.cula_mobile.utils.SharedPreferenceUtils;
 
@@ -13,14 +14,18 @@ import retrofit2.Response;
 
 public class DetailTaskPresenter {
     IDetailTaskView view;
+    private int idTask;
+    private Comment comment = new Comment();
+    private IApiEndpoint endpoint;
 
-    public DetailTaskPresenter(IDetailTaskView view) {
+    public DetailTaskPresenter(IDetailTaskView view, int idTask) {
         this.view = view;
+        this.idTask = idTask;
+        this.endpoint = ApiRetrofit.getInstance().create(IApiEndpoint.class);
     }
 
-    public void getDetailTask(int idTask) {
+    public void getDetailTask() {
         Log.e("koi", idTask + "");
-        IApiEndpoint endpoint = ApiRetrofit.getInstance().create(IApiEndpoint.class);
         Call<ResponseDetailTask> detailTaskCall = endpoint.detailTask(SharedPreferenceUtils
                 .getStringSharedPreferences("token", "detailTask"), idTask);
         detailTaskCall.enqueue(new Callback<ResponseDetailTask>() {
@@ -29,6 +34,7 @@ public class DetailTaskPresenter {
                 if (response.isSuccessful()) {
                     Log.e("koi", response.code() + "");
                     view.showDetailTask(response.body());
+                    view.showLabel(response.body().getLabel());
                     view.showSubtask(response.body().getSubtasks());
                     view.showComment(response.body().getComments());
                     view.showTaskTitle(response.body().getTaskName());
@@ -40,6 +46,32 @@ public class DetailTaskPresenter {
             @Override
             public void onFailure(Call<ResponseDetailTask> call, Throwable t) {
                 Log.e("koi", t.toString());
+            }
+        });
+    }
+
+    public void addComment(String content) {
+        String token = SharedPreferenceUtils.getStringSharedPreferences("token", "comment");
+        int idUser = SharedPreferenceUtils.getIntSharedPreferences("idUser", 0);
+        Log.e("koiComment", idUser + "");
+        Call<Comment> commentCall = endpoint.addComment(token, content, idTask, idUser);
+        commentCall.enqueue(new Callback<Comment>() {
+            @Override
+            public void onResponse(Call<Comment> call, Response<Comment> response) {
+                Log.e("koiComment", response.code()+"");
+                if (response.isSuccessful()) {
+                    comment.setComment(response.body().getComment());
+                    comment.setIdTask(response.body().getIdTask());
+                    comment.setIdUser(response.body().getIdUser());
+                    view.reload();
+                } else {
+                    Log.e("koiComment", "unsuccess");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Comment> call, Throwable t) {
+                Log.e("koiComment", t.toString());
             }
         });
     }
